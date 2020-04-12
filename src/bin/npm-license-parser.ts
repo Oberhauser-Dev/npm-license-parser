a385c2d875dbc65e76495d468d644c77327023de
Gustl22 <user.rebo@gmx.de>
2020-04-12 16:52:45 +0200

    More options

--- src/bin/npm-license-parser.ts
+++ src/bin/npm-license-parser.ts
@@ -7,7 +7,6 @@ import LicenseParser from "../lib/LicenseParser.js";
 const options: CrawlerOptions = {
   //exclude: ['.'],
   //json: 'licenses.json',
-  onlyDirectDependencies: true,
   //start: ['../..'],
   start: ['./'],
   //unknown: true,
@@ -19,11 +18,21 @@ const version = process.env.npm_package_version || '0';
 
 program
   .version(version)
-  .description("An example CLI for ordering pizza's")
-  .option('-j, --json <path>', 'Export License in specified path')
+  .description("An License CLI for displaying dependencies.")
+  .option('-t, --dependencies', 'Show only third-party licenses')
+  .option('--development', 'Show only development dependencies')
+  .option('-e, --exclude <path>', 'Path to a directory to be excluded from the search')
+  .option('-d, --onlyDirectDependencies', 'Show only direct dependencies licenses')
+  .option('-d, --production', 'Show only production dependencies licenses')
+  .option('-j, --json <path>', 'Export data as JSON to the given file')
   .option('-s, --start <path>', 'The start path to search')
   .parse(process.argv);
 
+if (program.dependencies) options.dependencies = true;
+if (program.development) options.development = true;
+if (program.exclude) options.exclude = program.exclude;
+if (program.onlyDirectDependencies) options.onlyDirectDependencies = true;
+if (program.production) options.production = true;
 if (program.start) options.start = program.start;
 const parser = new LicenseParser(options);
 if (program.json) parser.json(program.json);
